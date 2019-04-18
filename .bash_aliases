20df033e794bcc3f6d360d172b4207a92144944b
Christophe Goudet <goudetchristophe@laposte.net>
2019-04-18 09:37:22 +0200

    initial

---
+++ .bash_aliases
@@ -0,0 +1,3 @@
+alias em="emacs -nw"
+alias py="python3"
+alias clara_prod="sshpass -f ~/.claranet_prod ssh root@hapads001:hapcgt01@bastion.grita.fr"
---
+++ install.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove --purge pidgin sylpheed parole -y
+
+sudo apt-get install -y \
+python3-pip python3-dev postgresql \
+docker.io docker-compose \
+emacs vlc terminator youtube-dl \
+nautilus-dropbox owncloud-client keepassx jabref texlive-full zim \
+build-essential git virtualbox virtualenv 
+
+# for porblem with owncloud
+sudo apt install libgnome-keyring0 -y
+
+# library for pyodbc
+sudo apt-get install unixodbc-dev -y
+
+# install visualstudio code
+sudo dpkg -i *.deb
+sudo apt-get install -f
+
+# Cisco
+# sudo apt-get install lib32z1 lib32ncurses5 libpangox-1.0-0 network-manager-openconnect network-manager-pptp network-manager-vpnc network-manager-openvpn
+
+# tesseract for ocr
+# tesseract-ocr tesseract-ocr-fra
+# Add <policy domain="coder" rights="read" pattern="PDF" />
+# to /etc/ImageMagick-6/policy.xml
+
+# authorise non root users to run docker
+sudo usermod -aG docker $(whoami)
+
+sudo apt-get install libdvd-pkg -y
+sudo dpkg-reconfigure libdvd-pkg
+
+cp .bash_aliases .gitconfig ~/.
+source ~/.bashrc
