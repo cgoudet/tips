71280ff73de3bd189e3268e65b2ef15fa510e3b4
Christophe Goudet <goudetchristophe@laposte.net>
2019-04-29 13:46:11 +0200

    added vscode

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension garaemon.vscode-emacs-tab
+code --install-extension lextudio.restructuredtext
+code --install-extension ms-python.python
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension shardulm94.trailing-spaces
+code --install-extension t-yng.emacs-region
+
+
